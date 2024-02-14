06a5917a4987272f27a8114ca500f295aed78f7e
Animesh Anand <animeshanand@Animeshs-MacBook-Air.local>
2024-02-14 15:23:12 +0530

    mongodb cloud link added

--- data/database.js
+++ data/database.js
@@ -4,6 +4,6 @@ const connectDB = ()=>
 {
     mongoose.connect(process.env.MONGO_URI,{
     dbName:"backendapi"
-    }).then(()=>console.log("Database connected")).catch((e)=>console.log(e))
+    }).then((c)=>console.log(`Database connected with ${c.connection.host}`)).catch((e)=>console.log(e))
 }
     export default connectDB;
