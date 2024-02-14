64d9be118e8514a8079d1f4bad51c555c468b20c
Animesh Anand <animeshanand@Animeshs-MacBook-Air.local>
2024-02-14 12:00:42 +0530

    all setup done

--- controllers/user.js
+++ controllers/user.js
@@ -1,65 +1,68 @@
 import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
 
-const getAllUsers=async (req, res)=>{
+import { sendCookie } from "../utils/feature.js";
+import ErrorHandler from "../middlewares/error.js";
 
-    const users= await User.find({});
-    console.log(req.query);
-     const keyword = req.query.keyword;
-     console.log(keyword)
-      res.json({
-          success: true,
-          users
-      })
+    export const login = async (req,res,next)=>{
+        try {
+            const {email,password}=req.body;
+
+        let user= await User.findOne({email}).select("+password")
+        
+            if(!user) return next(new ErrorHandler("Invalid Email or Password",400))
+        
+        const isMatch=await bcrypt.compare(password,user.password);
+ 
+        if(!isMatch)
+        return next(new ErrorHandler("Invalid Email or Password",400))
+
+            sendCookie(user,res,`Welcome back ${user.name}`,200)
+        } catch (error) {
+           next(error) 
+        }
     }
+    export const logout=((req,res)=>{
+        
+        res.status(200).cookie("token","",{
+            expires:new Date(Date.now()),
+            sameSite:process.env.NODE_ENV === "Development"?"lax":"none" ,
+           secure:process.env.NODE_ENV === "Development"?false:true,
+        }).json({
+            success:true,
+            user:req.user
+        })
+    })
     export const register =async (req, res,next)=>{
-        const {name,email,password}=req.body;
-        
-            const users= await User.create({
-                name,email,password
-            })
-             
-              res.status(201).cookie("tempi","lol").json({
-                  success: true,
-                    message:"Registered Successfully"
-              })
+        try {
+            const {name,email,password}=req.body;
+        console.log("inside resgister");
+
+        let user = await User.findOne({email});
+        if(user){
+            return next(new ErrorHandler("User Already Exist",400))
+        }
+        const hashedPassword=await bcrypt.hash(password,10);
+
+        user= await User.create({name,email,password:hashedPassword})
+
+        sendCookie(user,res,"Registered Successfully",201)
+        } catch (error) {
+            next(error)
+        }
+            
           }
-          export const specialFunc=(req,res)=>{
-            res.json({
+         
+     export const getMyProfile=(req,res)=>{
+            const id="myid";
+           
+            res.status(200).json({
                 success:true,
-                message:"just joking"
+                user:req.user
             })
         }
-        export const getUserDetails=async (req,res)=>{
+
+        export default login;
+       
+        
     
-            const {id}=req.params
-        
-            const user=await User.findById(id)
-        
-            res.json({
-                success:true,
-                user
-            })
-        }
-        export const updateUser=async (req,res)=>{
-    
-            const {id}=req.params
-        
-            const user=await User.findById(id)
-        
-            res.json({
-                success:true,
-                message:"updated"
-            })
-        }
-        export const deleteUser=async (req,res)=>{
-    
-            const {id}=req.params
-        
-            const user=await User.findById(id)
-        // await user.remove();
-            res.json({
-                success:true,
-                message:"Deleted"
-            })
-        }
-    export default getAllUsers;
